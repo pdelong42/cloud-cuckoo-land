c5bfb404cb25e3a393efb46cfd2df70e7e02587b
Paul DeLong <Paul.DeLong@gmail.com>
2025-08-12 12:33:23 -0400

    first stab at making some better example code (it's buggy);

---
+++ AWS/test-stop-with-console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+import boto3
+
+# This library is in the current directory.  I don't know if that's
+# what Python considers a "best pracice", but it feels gross to me.
+# But I'm going to do that until I find a better way to do library
+# path management in this runtime.
+#
+from console import ConsoleToInstance
+
+# I'll write better arg processing later (I'm sure Python has a mod)
+if 2 != len( sys.argv ):
+    print( "ERROR: please provide an instance ID as the first (and only) arg" )
+    sys.exit()
+
+instanceId = sys.argv.pop()
+
+session = boto3.session.Session()
+client = session.client( service_name='ec2' )
+
+response = client.stop_instances( InstanceIds = [ instanceId ] )
+
+for x in response[ 'StoppingInstances' ]:
+    print( f'Transitioning instance {x['InstanceId']} from {x['PreviousState']['Name']} to {x['CurrentState']['Name']}' )
+
+ConsoleToInstance( instanceId )
