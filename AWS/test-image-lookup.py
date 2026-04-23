70740d58b19c963ba9175aac980fba45af3972e8
Paul DeLong <Paul.DeLong@gmail.com>
2026-04-22 22:02:09 -0400

    some cosmetic differences, and switched OS testing with;

--- AWS/test-image-lookup.py
+++ AWS/test-image-lookup.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 
+# Example:
+#
+#   ./test-image-lookup.py 2> stderr.json
+#
+# It prints results to STDOUT.  To troubleshoot problems:
+#
+#   jq -SC < stderr.json | less -RXi
+
 import sys
-import boto3
 
 from json import dumps
 from amiuniq import UniqueMachineImage
@@ -9,13 +16,18 @@ from amiuniq import UniqueMachineImage
 # The idea here is to specify enough fitler tokens that only one
 # result is returned.  Zero matches is considered a fatal error, and
 # more than one match is met with a non-fatal warning.
+#
+# To make the response deterministic, use a time period that is no
+# longer in flux.  For example: pick a month that just ended, and not
+# the month that you are currently in the middle of.  The same goes
+# for any time period, whether it's the year or the day).
 
-#umi = UniqueMachineImage( {
-#    'architecture': 'x86_64',
-#    'creation-date': '2026-01-*',
-#    'name': 'RHEL-*',
-#    'owner-id': '309956199498'
-#} )
+umi = UniqueMachineImage( {
+    'architecture': 'x86_64',
+    'creation-date': '2026-02-*',
+    'name': 'RHEL-*',
+    'owner-id': '309956199498'
+} )
 
 # Note about Amazon Linux: the year of release (2023, in this case),
 # is part of the name.  Don't let that confuse you; they've updated it
@@ -28,31 +40,30 @@ from amiuniq import UniqueMachineImage
 #    'owner-id': '137112412989'
 #} )
 
-umi = UniqueMachineImage( {
-    'architecture': 'x86_64',
-    'creation-date': '2026-01-*',
-    'name': 'Fedora-Cloud-Base-AmazonEC2.x86_64-43-*',
-    'owner-id': '125523088429'
-} )
+#umi = UniqueMachineImage( {
+#    'architecture': 'x86_64',
+#    'creation-date': '2026-02-*',
+#    'name': 'Fedora-Cloud-Base-AmazonEC2.x86_64-43-*',
+#    'owner-id': '125523088429'
+#} )
 
 if 1 > umi.size:
     print( "ABORT: no match found" )
     sys.exit( 1 )
 
 if 0 < umi.size:
-    print( umi.ID )
+    print( f'first match:\nAMI = {umi.ID}; name = {umi.name}' )
 
 if not 1 < umi.size:
     sys.exit( 0 )
 
 print( dumps( umi.images, default = str ), file = sys.stderr )
 
-print( "extra matches..." )
+print( "extra matches:" )
 
 for i in umi.images:
     AMI = i[ 'ImageId' ]
     name = i[ 'Name' ]
-    print( f'name = {name}' )
-    #print( f'AMI = {AMI}; name = {name}' )
+    print( f'AMI = {AMI}; name = {name}' )
 
 #            'Values' : [ 'al2023-ami-2023.7.20250527.1-kernel-6.1-x86_64' ]
