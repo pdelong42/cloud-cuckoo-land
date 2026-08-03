62e735c1d62b1b2d4983d0784428772a9d4d67f4
Paul DeLong <Paul.DeLong@gmail.com>
2026-08-03 15:34:09 -0400

    simple monthly cost and usage scraper;
    I just wanted to finally be able to get the output consisting of the two dollar
    amounts in the "Cost and usage" dashboard from the webUI home, and be able to
    print them out on the command-line without having to log-in.
    
    Perhaps I'll do more granular output at a later time.

---
+++ AWS/cost-estimates.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+#import sys
+import copy
+
+from json import dumps
+from boto3 import Session
+from time import localtime, mktime, strftime
+
+# for future consideration...
+#import datetime
+
+now = localtime()
+later = localtime( mktime( copy.replace( now, tm_mon = now.tm_mon + 1 ) ) )
+begin = strftime( '%Y-%m-01', now )
+today = strftime( '%Y-%m-%d', now )
+end = strftime( '%Y-%m-01', later )
+
+cetacean = Session().client( service_name = 'ce' )
+
+# I don't really understand why metrics are handled so differently by
+# the two API calls below, but I worked around it as best as I could.
+#
+metric = 'UNBLENDED_COST'
+metrics = [ 'UnblendedCost' ]
+
+response = cetacean.get_cost_and_usage(
+    Granularity = 'MONTHLY',
+    Metrics = metrics,
+    TimePeriod = { 'Start': begin, 'End': today } )
+
+#print( dumps( response, default = str ), file = sys.stderr )
+
+print( f'Costs so-far for this month:' )
+
+for r in response[ 'ResultsByTime' ]:
+
+    for m in metrics:
+
+        total = r[ 'Total' ][ m ]
+        amount = float( total[ 'Amount' ] )
+        unit = total[ 'Unit' ]
+
+        print( f'\t...for metric "{m}": {amount:#.2f} {unit}' )
+
+response = cetacean.get_cost_forecast(
+    Granularity = 'MONTHLY',
+    Metric = metric,
+    TimePeriod = { 'Start': today, 'End': end } )
+
+#print( dumps( response, default = str ), file = sys.stderr )
+
+total = response[ 'Total' ]
+amount = float( total[ 'Amount' ] )
+unit = total[ 'Unit' ]
+
+print( f'Costs forecast for this month:' )
+print( f'\t...for metric "{metric}": {amount:#.2f} {unit}' )
